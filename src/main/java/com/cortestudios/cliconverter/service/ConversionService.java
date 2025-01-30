dd6c89af29df2d7aa918ba071e8c5f8ddec025bd
jecortes <jecortes2304@gmail.com>
2025-01-30 15:53:25 +0100

    - Added debug log

--- src/main/java/com/cortestudios/cliconverter/service/ConversionService.java
+++ src/main/java/com/cortestudios/cliconverter/service/ConversionService.java
@@ -2,11 +2,13 @@ package com.cortestudios.cliconverter.service;
 
 import com.cortestudios.cliconverter.model.RequestModel;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.stereotype.Service;
 import java.util.Map;
 import java.util.Objects;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ConversionService {
@@ -19,6 +21,8 @@ public class ConversionService {
                 .sourceFormat(inputFormat)
                 .destinationFormat(outputFormat)
                 .build();
+
+        log.debug("Request: {}", message);
 
         Map<String, String> response = Map.of("completion",
                 Objects.requireNonNull(chatClient.prompt()
